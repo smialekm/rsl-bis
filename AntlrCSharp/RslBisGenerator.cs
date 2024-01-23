ab71c5dd671193656f33b0a8e0c6d8e5d36a7cff
smialek <michal.smialek@ee.pw.edu.pl>
2024-01-23 15:43:04 +0100

    TranslationIntermediary added

--- AntlrCSharp/RslBisGenerator.cs
+++ AntlrCSharp/RslBisGenerator.cs
@@ -1,8 +1,8 @@
 using System.Diagnostics.Contracts;
 using Antlr4.Runtime.Misc;
 
-public class RslBisGenerator : RslBisBaseVisitor<object> {
-    public override object VisitStart([NotNull] RslBisParser.StartContext context)
+public class RslBisGenerator : RslBisBaseVisitor<TranslationIntermediary> {
+    public override TranslationIntermediary VisitStart([NotNull] RslBisParser.StartContext context)
     {
         object childResult = base.VisitStart(context);
         return null; // TODO - generate code!!
